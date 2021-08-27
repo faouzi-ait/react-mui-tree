97991df7ee69cc96e28a014c4218478d4300cf21
Faouzi Aitelhara <faouzi.aitelhara@gmail.com>
2021-08-27 15:53:15 +0100

    Project Done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
+import React from "react";
+import TreeView from "@material-ui/lab/TreeView";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import ChevronRightIcon from "@material-ui/icons/ChevronRight";
+import TreeItem from "@material-ui/lab/TreeItem";
+import { Checkbox, FormControlLabel } from "@material-ui/core";
+import { data } from "./data/sampleData";
+
 import './App.css';
 
 function App() {
+  const [selected, setSelected] = React.useState([]);
+
+  function getChildById(node, id) {
+    let array = [];
+
+    function getAllChild(nodes) {
+      if (nodes === null) return [];
+      array.push(nodes.id);
+      if (Array.isArray(nodes.children)) {
+        nodes.children.forEach((node) => {
+          array = [...array, ...getAllChild(node)];
+          array = array.filter((v, i) => array.indexOf(v) === i);
+        });
+      }
+      return array;
+    }
+
+    function getNodeById(nodes, id) {
+      if (nodes.id === id) {
+        return nodes;
+      } else if (Array.isArray(nodes.children)) {
+        let result = null;
+        nodes.children.forEach((node) => {
+          if (!!getNodeById(node, id)) {
+            result = getNodeById(node, id);
+          }
+        });
+        return result;
+      }
+
+      return null;
+    }
+
+    return getAllChild(getNodeById(node, id));
+  }
+
+  function getOnChange(checked, nodes) {
+    const allNode = getChildById(data, nodes.id);
+    let array = checked
+      ? [...selected, ...allNode]
+      : selected.filter((value) => !allNode.includes(value));
+
+    array = array.filter((v, i) => array.indexOf(v) === i);
+
+    setSelected(array);
+  }
+
+  const renderTree = (nodes) => (
+    <TreeItem
+      key={nodes.id}
+      nodeId={nodes.id}
+      label={
+        <FormControlLabel
+          control={
+            <Checkbox
+              checked={selected.some((item) => item === nodes.id)}
+              onChange={(event) =>
+                getOnChange(event.currentTarget.checked, nodes)
+              }
+              onClick={(e) => e.stopPropagation()}
+              color = "primary"
+            />
+          }
+          label={<>{nodes.name}</>}
+          key={nodes.id}
+        />
+      }
+    >
+      {Array.isArray(nodes.children)
+        ? nodes.children.map((node) => renderTree(node))
+        : null}
+    </TreeItem>
+  );
+
+  // LIST OF SELECTED ITEM
+  console.log(selected)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App">    
+      <TreeView
+        defaultCollapseIcon={<ExpandMoreIcon />}
+        defaultExpanded={["0"]}
+        defaultExpandIcon={<ChevronRightIcon />}
+      >
+        {renderTree(data)}
+      </TreeView>
     </div>
   );
 }
